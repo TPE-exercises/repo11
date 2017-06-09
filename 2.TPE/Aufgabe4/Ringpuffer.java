6a4fdfd19ebbb66a52ad9467ba9f1b115bef5e17
musti00 <1610362@stud.hs-mannheim.de>
2017-06-09 17:26:12 +0200

    Add files via upload

--- 2.TPE/Aufgabe4/Ringpuffer.java
+++ 2.TPE/Aufgabe4/Ringpuffer.java
@@ -1,99 +1,70 @@
 package de.hsMannheim.informatik.tpe.ss17.uebung04.gruppe1;
 
 public class Ringpuffer extends Thread {
-	
+
 	boolean leer = true;
 	private Object array[];
 	private int pointer;
-	boolean isEmpty(){
-		return array[array.length-1]== null;
-	}
-	public void run(){
-		for (int i = 0; i <100; i++) {
-			
-			try {
-				
-				while(array[0] == null){
-					int randomZahl = (int)(Math.random()*10);
-					put(randomZahl);
-					printArray();
-				}
-				
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try{
-				while(!isEmpty()){
-				get();
-				}
-				printArray();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-
+	private int counter;
 
 	public Ringpuffer(int size) {
 
 		array = new Object[size];
 		pointer = size + 1;
+		counter = 0;
 	}
-	
-	
 
 	synchronized public void put(Object element) throws InterruptedException {
 		pointer--;
-		while(array[0] != null){
-//			System.out.println("Ringpuffer ist voll" );
+		while (array[0] != null) {
+			 System.out.println("Ringpuffer ist voll" );
 			wait();
-		}if(array[0]==null){
-			 
+		}
+
+		notifyAll();
 		for (int i = array.length - 1; i >= 0; i--) {
 			if (array[i] == null) {
 				array[i] = element;
-				
 				break;
 			}
 
 		}
-		notifyAll();
-		}
+
 	}
 
 	synchronized public Object get() throws InterruptedException {
-		Object element=0;
-		while(array[array.length-1]==null){
-//			System.out.println("Kein element im Ringpuffer");
+		Object element = 0;
+		while (array[array.length - 1] == null) {
+			 System.out.println("Kein element im Ringpuffer");
 			wait();
-		}if(array[array.length-1]!=null){
+		}
+
+		notifyAll();
 		element = array[array.length - 1];
-		notifyAll(); 
+
 		Object newArray[] = new Object[array.length];
-		int c = 0;
-	
+		int c = counter;
 		for (int i = pointer; i < array.length; i++, c++) {
 			newArray[i] = array[c];
 		}
 		array = newArray;
-		}
-		pointer++;
-		return element;
-		
 
-	} 
+		pointer++;
+		counter++;
+
+		return element;
+	}
 
 	void printArray() {
 		for (int i = 0; i < array.length; i++) {
-			System.out.print(  " , "+array[i]);
+			System.out.print(" , " + array[i]);
 		}
 		System.out.println();
-		
 
 	}
-	void signalfull(){
-		
+
+	boolean isEmpty() {
+		return array[array.length - 1] == null;
 	}
+
 }
--- 2.TPE/Aufgabe4/TestRP.java
+++ 2.TPE/Aufgabe4/TestRP.java
@@ -1,16 +1,26 @@
 package de.hsMannheim.informatik.tpe.ss17.uebung04.gruppe1;
 
+import java.util.Scanner;
+
 public class TestRP {
 
 	public static void main(String[] args) throws InterruptedException {
-		Ringpuffer obj = new Ringpuffer(4);
-		obj.start();
-        
-		
-	
 
-	
+		Ringpuffer rp = new Ringpuffer(4);
+		rp.put(7);
+		rp.put(10);
+		rp.put(15);
+		rp.put(20);
+
+		rp.get();
+		rp.get();
+		rp.get();
+		rp.get();
+		rp.get();
 		
+		
+		rp.printArray();
+
 	}
 
 }
